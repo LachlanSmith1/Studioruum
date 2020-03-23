2be3f86d7783a1ce1aaa9137eec44ab53e80d8c5
Daniel Ponturo <47391751+danielponturo@users.noreply.github.com>
2020-03-23 23:37:56 +0000

    Add files via upload

--- 04_Implementation/classes/Flashcard.java
+++ 04_Implementation/classes/Flashcard.java
@@ -4,65 +4,74 @@ import java.io.IOException;
 
 public class Flashcard extends Resource
 {
-	
+
 	//the text kept on each side of the flashcard
 	private String frontContent, backContent;
-	
+
 	//the ID for any other resources it is attached to
 	private int dictionaryID, quizID;
-	
-	
+
+
 	public Flashcard(String name)
 	{
-		
+
 		super(name);
-		
+
 		//DUMMY DATA FOR FLASHCARDS
-		
+
 		frontContent = "What is the capital of Belgium?";
 		backContent = "Brussels";
-		
-	}
-	
-	//gets the front content
-	public String getFront()
-	{
 
+	}
+
+	// getters and setter for frontContent, backContent and dictionaryID
+	public String getFront() {
 		return frontContent;
-		
 	}
-	
-	//gets the back content
-	public String getBack()
-	{
-		
+
+	public void setFront(String content) {
+		this.frontContent = content;
+	}
+
+	public String getBack() {
 		return backContent;
-		
 	}
-	
+
+	public void setBack(String content) {
+		this.backContent = content;
+	}
+
+	public int getDict() {
+		return dictionaryID;
+	}
+
+	public void setDict(int ID) {
+		this.dictionaryID = ID;
+	}
+
 	public static void main(String[] args)
 	{
-		
+
 		Flashcard exFlashcard = new Flashcard("exFlashcard");
-		
+
 		exFlashcard.writeResource();
-		
+
 	}
-	
+
 	public void writeResource()
 	{
-		
+
 		//the name of the file which will be made at the current directory
-		String name = resourceName + ".txt"; 
-		
+		String name = resourceName + ".txt";
+
 		try
 		{
-			
+
 			//create a file object at the current directory
 			File resourceFile = new File(name);
-			
+
 			boolean fileExists = resourceFile.createNewFile();
-			
+
 			//if the file didn't exist then creation was successful
 			if (fileExists)
 			{
@@ -72,27 +81,27 @@ public class Flashcard extends Resource
 			{
 				System.out.println("Flashcard File Already Exists.");
 			}
-			
-			//create a FileWriter object to write to the new 
+
+			//create a FileWriter object to write to the new
 			FileWriter resourceWriter = new FileWriter(name, false);
-			
+
 			//write the meta information to the file seperated by scarabs
 			resourceWriter.write(frontContent + "\n");
 			resourceWriter.write(" ¤¤ " + "\n");
 			resourceWriter.write(backContent);
-			
+
 			//close the writer once it's finished
 			resourceWriter.close();
-			
-			
+
+
 		}
 		catch(IOException e)
 		{
-			
+
 			e.printStackTrace();
-			
+
 		}
-		
+
 	}
-	
+
 }
--- 04_Implementation/classes/Quiz.java
+++ 04_Implementation/classes/Quiz.java
@@ -7,42 +7,42 @@ public class Quiz extends Resource
 {
 	// arraylist to hold the flashcards in the quiz
 	ArrayList<Flashcard> theQuiz = new ArrayList<Flashcard>();
-	
+
 	//the current question and answer for the selected flashcard in the quiz
 	private String question, answer, topic;
-	
+
 	public Quiz(String name, String Topic)
 	{
-		
+
 		super(name);
-		
+
 		topic = Topic;
-		
+
 	}
-	
+
 	//adds a flashcard to the quiz
 	public void addFlashcard(Flashcard card)
 	{
-		
+
 		theQuiz.add(card);
-		
+
 	}
-	
+
 	//writes the flashcards in the quiz to a file. requires at least one flashcard in the quiz
 	public void writeResource()
 	{
-		
+
 		//the name of the file which will be made at the current directory
-		String name = resourceName + ".txt"; 
-		
+		String name = resourceName + ".txt";
+
 		try
 		{
-			
+
 			//create a file object at the current directory
 			File resourceFile = new File(name);
-			
+
 			boolean fileExists = resourceFile.createNewFile();
-			
+
 			//if the file didn't exist then creation was successful
 			if (fileExists)
 			{
@@ -52,10 +52,10 @@ public class Quiz extends Resource
 			{
 				System.out.println("Quiz File Already Exists.");
 			}
-			
-			//create a FileWriter object to write to the new 
+
+			//create a FileWriter object to write to the new
 			FileWriter resourceWriter = new FileWriter(name, false);
-			
+
 			//go through each flashcard in the quiz and write it to the file
 			for(int i=0;i<theQuiz.size();i++){
 				//write the meta information to the file seperated by scarabs
@@ -66,19 +66,19 @@ public class Quiz extends Resource
 			if(theQuiz.size()==0){
 				resourceWriter.write("Empty Quiz");
 			}
-			
+
 			//close the writer once it's finished
 			resourceWriter.close();
-			
-			
+
+
 		}
 		catch(IOException e)
 		{
-			
+
 			e.printStackTrace();
-			
+
 		}
-		
+
 	}
 
 }
