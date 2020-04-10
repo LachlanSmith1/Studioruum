c4e6726d6b16b87bd613fe1e67a9b7d8510a7edf
Daniel Ponturo <47391751+danielponturo@users.noreply.github.com>
2020-04-10 21:03:31 +0100

    Added methods for notes page

--- 04_Implementation/interface/studioruum_v0.1/src/gui/Controller.java
+++ 04_Implementation/interface/studioruum_v0.1/src/gui/Controller.java
@@ -1,20 +1,25 @@
 package gui;
+
 import java.io.IOException;
+import java.util.*;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
-
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 public class Controller {
     
     public String accountType="";
+    LocalDB locDB = new LocalDB();
 
     //checks if the username exists in the database
     public Boolean isUnique(String username){
@@ -80,6 +85,7 @@ public class Controller {
         window.setScene(destScene);
         window.show();
     }
+
     public void goFlashcard(ActionEvent event) throws IOException{
         Parent dest = FXMLLoader.load(getClass().getResource("view_flashcard.fxml"));
         Scene destScene = new Scene(dest);
@@ -98,13 +104,163 @@ public class Controller {
         window.show();
     }
 
-    public void goNotes(ActionEvent event) throws IOException{
+    // Called to prepare Notes page
+    public void goNotes(ActionEvent event) throws IOException {
+        // Get Stage info and set destination Scene
         Parent dest = FXMLLoader.load(getClass().getResource("view_notes.fxml"));
         Scene destScene = new Scene(dest);
-        //This line gets the Stage information
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         window.setScene(destScene);
+
+        // Lookup in the Scene for ComboBox, fetch items from local DB and add them
+        ComboBox noteDropDown = (ComboBox) destScene.lookup("#noteDrpDwn");
+        List<Note> notes = locDB.allNotes();
+        ObservableList<Note> observableNotes = FXCollections.observableList(notes);
+        noteDropDown.setItems(observableNotes);
+
+        // Updates item in ComboBox to show only their title instead of their full instance
+        Callback<ListView<Note>, ListCell<Note>> factory = lv -> new ListCell<Note>() {
+
+            @Override
+            protected void updateItem(Note item, boolean empty) {
+                super.updateItem(item, empty);
+                setText(empty ? "" : item.getTitle());
+            }
+
+        };
+
+        noteDropDown.setCellFactory(factory);
+        noteDropDown.setButtonCell(factory.call(null));
+
+        // Finally display window
         window.show();
+    }
+
+    // Displays selected Note in TextField and TextArea of the notes page
+    public void displayNote(ActionEvent event) throws IOException{
+        // Get Stage and Scene info
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = window.getScene();
+
+        // Lookup in the Scene for ComboBox, get selected item
+        ComboBox noteDropDown = (ComboBox) scene.lookup("#noteDrpDwn");
+        Note selected = (Note) noteDropDown.getSelectionModel().getSelectedItem();
+
+        // Set displayed title to selected note's title
+        TextField noteTitle = (TextField) scene.lookup("#noteTitle");
+        noteTitle.setText(selected.getTitle());
+
+        // Set displayed content to selected note's content
+        TextArea noteContent = (TextArea) scene.lookup("#noteContent");
+        noteContent.setText(selected.getContent());
+
+        // Set "Save" button (alter note) to visible
+        Button updateNote = (Button) scene.lookup("#updateNote");
+        updateNote.setVisible(true);
+    }
+
+    // Makes the note title and the note content editable when "Edit" button clicked
+    public void editNote(ActionEvent event) throws IOException{
+        // Get Stage and Scene info
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = window.getScene();
+
+        // Lookup in the Scene for TextField (note title) and make it editable
+        TextField noteTitle = (TextField) scene.lookup("#noteTitle");
+        noteTitle.setEditable(true);
+
+        // Lookup in the Scene for TextArea (note content) and make it editable
+        TextArea noteContent = (TextArea) scene.lookup("#noteContent");
+        noteContent.setEditable(true);
+    }
+
+    // Deletes a note from local DB (if user previously selected one
+    public void deleteNote(ActionEvent event) throws IOException{
+        // Get Stage and Scene info
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = window.getScene();
+
+        // Lookup in the Scene for ComboBox, get selected item
+        ComboBox noteDropDown = (ComboBox) scene.lookup("#noteDrpDwn");
+        Note selected = (Note) noteDropDown.getSelectionModel().getSelectedItem();
+
+        // Try to delete note if user selected one
+        if (selected != null) {
+            // Create and show deletion alert
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Note Deletion");
+            alert.setHeaderText("Trying to delete a note...");
+            alert.setContentText("Are you sure you want to delete this note? You will not be able to recover it in case of deletion");
+            Optional<ButtonType> result = alert.showAndWait();
+
+            // Delete note in case user clicks "OK", reload window
+            if (result.get() == ButtonType.OK) {
+                locDB.deleteNote(selected.getDict());
+                goNotes(event);
+            }
+        }
+
+        // Show Error alert in case no note selected
+        else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Delete Error");
+            alert.setHeaderText("No note to delete!");
+            alert.setContentText("You have to select a note to delete it!");
+
+            alert.showAndWait();
+        }
+    }
+
+    // Saves the note as a new note in local DB
+    public void saveNote(ActionEvent event) throws IOException {
+        // Get Stage and Scene info
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = window.getScene();
+
+        // Lookup in the Scene for note title and retrieve it
+        TextField noteTitle = (TextField) scene.lookup("#noteTitle");
+        String title = noteTitle.getText();
+
+        // Lookup in the Scene for note content and retrieve it
+        TextArea noteContent = (TextArea) scene.lookup("#noteContent");
+        String content = noteContent.getText();
+
+        // Save note and reload window
+        locDB.saveNote(title, content);
+        goNotes(event);
+    }
+
+    // Updates record of user select note in local db
+    public void updateNote(ActionEvent event) throws IOException {
+        // Get Stage and Scene info
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = window.getScene();
+
+        // Lookup in the Scene for ComboBox, get selected item's note_id
+        ComboBox noteDropDown = (ComboBox) scene.lookup("#noteDrpDwn");
+        Note selected = (Note) noteDropDown.getSelectionModel().getSelectedItem();
+        int noteId = selected.getDict();
+
+        // Lookup in the Scene for note title and retrieve it
+        TextField noteTitle = (TextField) scene.lookup("#noteTitle");
+        String title = noteTitle.getText();
+
+        // Lookup in the Scene for note content and retrieve it
+        TextArea noteContent = (TextArea) scene.lookup("#noteContent");
+        String content = noteContent.getText();
+
+        // Create and show overwrite alert
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Overwrite");
+        alert.setHeaderText("Trying to overwrite an existing note...");
+        alert.setContentText("Are you sure you want to overwrite this note? Its previous content will be completely replaced");
+        Optional<ButtonType> result = alert.showAndWait();
+
+        // Delete note in case user clicks "OK", reload window
+        if (result.get() == ButtonType.OK) {
+            locDB.updateNote(noteId, title, content);
+            goNotes(event);
+        }
     }
 
     public void goBack(ActionEvent event) throws IOException{
